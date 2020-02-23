beb3d38f033cdf01088b10beb8f64ec0dfc7c836
jasonephraim <jason@digitalcrafts.com>
2020-02-22 21:30:46 -0600

    added exercise 2 and completed

---
+++ 1/ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored
+
+print("I could have coded like this") # and the coment after is ignored.
+
+# You can also yse the comment to "disable" or comment out code:
+# print("this won't run")
+
+print("This will run.")
