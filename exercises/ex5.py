7c21a59f9330d234213ed2ca2968616274af42d7
jasonephraim <jason@digitalcrafts.com>
2020-02-24 22:19:32 -0600

    replaced ) with }

--- exercises/ex5.py
+++ exercises/ex5.py
@@ -13,4 +13,4 @@ print("Actually that's not too heavy")
 print(f"He's got {my_eyes} and {my_hair}.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height} and {my_weight) I get {total}")
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
