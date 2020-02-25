2fbf22a6104e7ecd3abc8203d22c50f184a3d54f
jasonephraim <jason@digitalcrafts.com>
2020-02-24 20:31:14 -0600

    added period

--- exercises/ex2.py
+++ exercises/ex2.py
@@ -1,5 +1,5 @@
 # A comment, this is so you can read your program later.
-# Anything after the # is ignored
+# Anything after the # is ignored.
 
 print("I could have coded like this") # and the coment after is ignored.
 
