9db362efa1bebf9ea273f8d78a0b4acf4b1052e5
jasonephraim <jason@digitalcrafts.com>
2020-02-24 21:11:15 -0600

    addedmex5

---
+++ exercises/ex5.py
@@ -0,0 +1,16 @@
+my_name = "Jason Ephraim"
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = "blue"
+my_teeth = "white"
+my_hair = "brown"
+
+print("Let's talk about {my_name}.")
+print("He's {my_height} inches tall.")
+print("He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy")
+print("He's got {my_eyes} and {my_hair}."
+print("His teeth are usually {my_teeth} depending on the coffee.")
+total = my_age + my_height + my_weight
+print("If I add {my_age}, {my_height} and {my_weight) I get {total}")
